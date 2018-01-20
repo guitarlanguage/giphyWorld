88419aa0ed2cf9fe05a4946be40816dd2f4aa093
Tom LaChance <guitarlanguageseries@gmail.com>
2018-01-19 22:42:24 -0600

    page functions, however could be more efficient.  still need to make the giphs be able to from moving to still and vice verse

--- app.js
+++ app.js
@@ -45,19 +45,7 @@ $(document).ready(function() {
         var topicButton = $("<button>" + queryTerm + "</button>");
         topicButton.attr("data-animal", queryTerm);
         $("#buttons").append(topicButton);
-    //==have the buttons coordinate with the giphy api========
-        console.log(topics);
-
-    });
-
-    for(var i = 0; i < topics.length; i++){
-        console.log("arrayloooooopo: " + topics[i]);
-        var preFabBtn = $("<button>" + topics[i] + "</button>");
-        preFabBtn.attr("data-animal", topics[i]);
-        $("#buttons").append(preFabBtn);
-
-
-
+        //==have the buttons coordinate with the giphy api========
         $("button").on("click", function() {
             var x = $(this).data("animal");
             console.log(x);
@@ -77,8 +65,44 @@ $(document).ready(function() {
                     animalDiv.append(p);
                     animalDiv.append(animalImg);
                     $("#gifsGoHere").prepend(animalDiv);
+
                 }
             });
         });
+    });
+
+
+
+    for (var i = 0; i < topics.length; i++) {
+        console.log("arrayloooooopo: " + topics[i]);
+        var preFabBtn = $("<button>" + topics[i] + "</button>");
+        preFabBtn.attr("data-animal", topics[i]);
+        $("#buttons").append(preFabBtn);
+
     }
+
+    $("button").on("click", function() {
+        var x = $(this).data("animal");
+        console.log(x);
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            x + "&api_key=7bBdC4gBmRiNNZ24sbtPXdTF2P5OByBI&limit=10";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            // console.log(response);
+            for (var i = 0; i < response.data.length; i++) {
+                var animalDiv = $("<div>");
+                var p = $("<p>").text("Rating: " + response.data[i].rating);
+                var animalImg = $("<img>");
+                animalImg.attr("src", response.data[i].images.fixed_height.url);
+                animalDiv.append(p);
+                animalDiv.append(animalImg);
+                $("#gifsGoHere").prepend(animalDiv);
+
+            }
+        });
+    });
+
 });
